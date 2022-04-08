05d1257bfb0cb2bcb10e953bf5ccaa093341ec46
liuwang <1139765630@qq.com>
2022-04-09 01:09:51 +0800

    second_commit

--- src/main/java/com/liuwang/git/GitTest.java
+++ src/main/java/com/liuwang/git/GitTest.java
@@ -3,5 +3,6 @@ package com.liuwang.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("HelloGit");
+        System.out.println("HelloGit2");
     }
 }
