656fce0144f25331a46b859b3e0235a6df75ab34
liuwang <1139765630@qq.com>
2022-04-09 17:47:10 +0800

    PushTest_commit

--- src/main/java/com/liuwang/git/GitTest.java
+++ src/main/java/com/liuwang/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("HelloGit");
         System.out.println("HelloGit4");
         System.out.println("PushTest");
+        System.out.println("GiteePushTest");
     }
 }
